f1aaf4ec3f50912ef21863d4eccc63e732058e4e
Sócrates Díaz Severino <sdsxd1800@gmail.com>
2015-05-04 21:17:20 -0400

    Showing the forecast detail on list item click

--- app/src/main/java/com/socratesdiaz/sunshine/ForecastFragment.java
+++ app/src/main/java/com/socratesdiaz/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.socratesdiaz.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -81,6 +84,17 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String textForecast = parent.getAdapter().getItem(position).toString();
+                /*int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(parent.getContext(), textForecast, duration).show();*/
+                Intent detailIntent = new Intent(parent.getContext(), DetailActivity.class);
+                detailIntent.putExtra(Intent.EXTRA_TEXT, textForecast);
+                startActivity(detailIntent);
+            }
+        });
 
         return rootView;
     }
